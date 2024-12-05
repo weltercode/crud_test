5acacf83eb5f4a25cb042662dcc77debc0379c26
welter <weltercode@yahoo.com>
2024-12-05 23:50:09 +0200

    Templates working now

--- internal/transport/rest/handler.go
+++ internal/transport/rest/handler.go
@@ -24,33 +24,31 @@ func NewHandler(router *mux.Router) *Handler {
 }
 
 // BaseHandler renders the base layout and dynamically includes specific content
-// func (h *Handler) BaseHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.WriteHeader(http.StatusOK)
+func (h *Handler) BaseHandler(w http.ResponseWriter, r *http.Request, data map[string]interface{}) {
+	w.WriteHeader(http.StatusOK)
 
-// 	// Dynamically include content template name
-// 	data["Content"] = content
-
-// 	// Render the base layout (e.g., index.html)
-
-// 	err := h.tmpl.ExecuteTemplate(w, "base.html", data)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	}
-// }
+	if err := h.tmpl.Execute(w, data); err != nil {
+		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 // HomeHandler handles the "/" route and injects specific content
 func (h *Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	h.tmpl, err = template.ParseFiles("templates/base.html", "templates/header.html", "templates/footer.html", "templates/home.html")
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
 	data := map[string]interface{}{
 		"Title":       "Welcome to simple task tracker",
 		"Description": "Manage your tasks efficiently",
 		"newTaskHref": h.getHrefByRouteName("task_new").String(),
 	}
-	err := h.tmpl.ExecuteTemplate(w, "base.html", data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	//h.BaseHandler(w, r, "home.html", data)
+
+	h.BaseHandler(w, r, data)
 }
 
 // TasksListHandler handles the "/tasks" route
@@ -68,25 +66,21 @@ func (h *Handler) TasksListHandler(w http.ResponseWriter, r *http.Request) {
 
 // TaskViewHandler handles the "/task/{id}" route
 func (h *Handler) TaskViewHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
 	vars := mux.Vars(r) // Get URL variables
 	taskID := vars["id"]
 
+	h.tmpl, err = template.ParseFiles("templates/base.html", "templates/header.html", "templates/footer.html", "templates/edit.html")
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	data := map[string]interface{}{
 		"Title":   "Task Details",
 		"TaskID":  taskID,
 		"Message": "Here are the details of your task.",
 	}
-	var err error
-
-	err = h.tmpl.ExecuteTemplate(w, "edit.html", data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	err = h.tmpl.ExecuteTemplate(w, "base.html", data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	h.BaseHandler(w, r, data)
 }
 
 // Helper function to get URLs for routes
